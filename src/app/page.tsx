9934f32e3a01017f30082278d93329d4e5497126
HermantoAng <hermantoang722@gmail.com>
2023-10-19 02:10:50 +0700

    TypedEffect Still Have problem FIXED IT!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 'use client'
 import React from 'react';
-import Typed from 'typed.js';
 
 import Image from 'next/image'
+// import TypeEffect from './components/typedAnimation/page'
+
 
 export default function Home() {
   return (
@@ -14,7 +15,10 @@ export default function Home() {
         <div className='bg-black/[.80] w-full h-full absolute'></div>
         <div className='w-full h-full z-10 absolute '> 
           <div className='flex justify-center items-center h-[80%]'>
-            <TypedEffect/>
+            {/* Animation Effect on text solve problem*/}
+            {/* <TypeEffect/> */}
+
+            <h1 className='text-white'>HOOMNA</h1>
           </div>
         </div>
       </div>
@@ -79,28 +83,3 @@ export default function Home() {
   )
 }
 
-
-function TypedEffect(props :any) {
-  // Create reference to store the DOM element containing the animation
-  const el = React.useRef(null);
-
-  React.useEffect(() => {
-    const typed = new Typed(el.current, {
-      strings: ['<i>We Are Hoomans</i> we will be.', '&amp; and You?.'],
-      typeSpeed: 70,
-    loop: true,
-    backSpeed: 30,
-    });
-
-    return () => {
-      // Destroy Typed instance during cleanup to stop animation
-      typed.destroy();
-    };
-  }, []);
-
-  return (
-    <div>
-      <span className='text-white text-3xl' ref={el} />
-    </div>
-  );
-}
