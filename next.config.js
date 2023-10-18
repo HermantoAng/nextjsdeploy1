c610446961587fc3b64a5c7255a3ff8dcd731c0f
HermantoAng <hermantoang722@gmail.com>
2023-10-19 02:31:00 +0700

    del the typed animation

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    reactStrictMode: true,
+    distDir: 'dist',
+    output: 'export',
+
     images : {
         remotePatterns : [
             {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 
 import Image from 'next/image'
-// import TypeEffect from './components/typedAnimation/page'
 
 
 export default function Home() {
